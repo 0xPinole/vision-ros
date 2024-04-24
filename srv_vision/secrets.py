d7bcad0ed923bd34f70f0619e47dd4f574ac0107
0xPinole <the.pinole@gmail.com>
2024-04-24 17:23:54 -0600

    feature

---
+++ srv_vision/secrets.py
@@ -0,0 +1,9 @@
+"""WIP."""
+
+
+class Credentials_aws:
+    """Credentials for aws."""
+
+    _endpoint = (
+        "https://s3zfpd9nk1.execute-api.us-east-2.amazonaws.com/default/inventorylambda"
+    )
