3cc44b9cf9d058c069b3ba152f69874be14fe41a
0xPinole <0xPinole@gmail.com>
2024-05-08 13:17:44 -0600

    closes #41; added as package custom interfaces for DataParams & VisionParams.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_name = "srv_vision"
 setup(
     name=package_name,
     version="0.0.0",
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
