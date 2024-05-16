41cec3dd6aedcf79d77f3780c6504d45ce5d0d09
0xPinole <0xPinole@gmail.com>
2024-05-16 14:07:44 -0600

    rebase service vision main

---
+++ srv_vision/objects.py
@@ -0,0 +1,11 @@
+"""Class owner of data format for objects."""
+class Detection:
+    y: int = None
+    aisle: int = None
+    shelf: int = None
+    code_name: str = None
+
+
+class MechanismParameters:
+    maximum_height: int = 360
+    minimun_height: int = 360 
---
+++ srv_vision/tests/process_node.py
@@ -0,0 +1,49 @@
+from vision_interfaces.srv import Procedure
+from vision_interfaces.msg import PositionalFrame
+from std_msgs.msg import Int8
+from geometry_msgs.msg import Pose2D
+import numpy as np
+
+class Procedure(Node):
+    """Ros node subscriber to information and trigger of task.
+
+    This node when needed, enables a process which capture each frame
+    and getting a positional status, send to evaluation node the request
+    to predict the model for the products on frame.
+    """
+    self.evaluate = False
+    self.aisle = -1
+    self.shelf = -1
+    self.frame = None
+
+    def __init__(self):
+        """Set a service who enables or disable the connection.
+
+        For the case is enable, the scissors subscriptions now the node will
+        publish the camera frame on the evaluation topic with custom message.
+        """
+        super().__init__("procedure_node")
+        self.service_setter = self.create_service(Procedure, "procedure_init", self.service_callback)
+        self.evaluate_pub = self.create_publisher(PositionalFrame, "evaluation", 1000)
+        self.scissors_sub = self.create_subscription(Pose2D, "scissor_movement_pub", self.scissors_callback, 1)
+        self.camera_sub = self.create_subscription(Int8, "camera_pub", self.camera_callback, 1)
+
+        self.positionalframe_msg = PositionalFrame()
+
+    def scissors_callback(self, scissors_position):
+        if self.evaluate:
+            self.positionalframe_msg.frame = np.array(self.frame).flatten()
+            self.positionalframe_msg.y = scissors_position.y
+
+            self.evaluate_pub.publish(positionalframe_msg)
+
+    def camera_callback(self, frame):
+        self.frame = frame.frame
+
+    def service_callback(self, request, response):
+        self.evaluate = request.capture
+        self.positionalframe_msg.aisle = request.aisle
+        self.positionalframe_msg.shelf = request.shelf
+
+        response.status_code = 1
+        return response
